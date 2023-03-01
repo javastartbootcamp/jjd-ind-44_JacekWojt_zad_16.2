06703850429366a1b9ec6a1ec1cc93527c9ac63b
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-03-02 00:15:31 +0100

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,15 @@
 package pl.javastart.task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    public static final int STRING_LENGTH = 10;
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -10,7 +17,46 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // uzupełnij rozwiązanie. Korzystaj z przekazanego w parametrze scannera
+        String dateEntered = collectingDate(scanner);
+        showingResult(dateEntered);
+    }
+
+    private static void showingResult(String dateEntered) {
+        List<String> patterns = Arrays.asList("yyyy-MM-dd HH-mm-ss", "yyyy-MM-dd HH:mm", "dd.MM.yyyy HH:mm:ss");
+        if (dateEntered.length() == STRING_LENGTH) {
+            dateEntered += " 00:00";
+        }
+        boolean added = false;
+        for (String pat : patterns) {
+            try {
+                DateTimeFormatter pattern = DateTimeFormatter.ofPattern(pat);
+                LocalDateTime localDateTime = LocalDateTime.parse(dateEntered, pattern);
+                LocalDateTime londonTime = localDateTime.minusHours(1);
+                LocalDateTime losAngelesTime = localDateTime.minusHours(9);
+                LocalDateTime sydneyTime = localDateTime.plusHours(10);
+                System.out.println("Czas lokalny: " + convertToString(localDateTime));
+                System.out.println("UTC: " + convertToString(londonTime));
+                System.out.println("Londyn: " + convertToString(londonTime));
+                System.out.println("Los Angeles: " + convertToString(losAngelesTime));
+                System.out.println("Sydney: " + convertToString(sydneyTime));
+                added = true;
+            } catch (DateTimeParseException e) {
+                //ignore
+            }
+        }
+        if (!added) {
+            System.out.println("Niepoprawny format daty.");
+        }
+    }
+
+    private static String convertToString(LocalDateTime localDateTime) {
+        String localD = localDateTime.toString();
+        return localD.replace('T', ' ');
+    }
+
+    private static String collectingDate(Scanner scanner) {
+        System.out.println("Podaj datę:");
+        return scanner.nextLine();
     }
 
 }
